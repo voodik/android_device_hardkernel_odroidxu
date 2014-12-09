b1343896bcf24e363789564f9f25bde1b2789cb3
root <voodik.am@gmail.com>
2014-12-09 21:39:56 +0400

    update audio_codec

--- include/audio_codec.h
+++ include/audio_codec.h
@@ -29,99 +29,78 @@ typedef struct AudioMixer_tag {
 //  : You have to modify.
 //
 const AudioMixer device_out_SPK[] = {
-#if defined (USES_I2S_AUDIO) || defined (USES_PCM_AUDIO)
-    {"Right Headphone Mux", 1}, // "Mixer","DAC"
-    {"Left Headphone Mux", 1},  // "Mixer","DAC"
-    {"DAC1R Mixer AIF1.1 Switch", 1}, // "OFF", "ON"
-    {"DAC1L Mixer AIF1.1 Switch", 1}, // "OFF", "ON"
-    {"DAC1 Switch", 1}, // "OFF OFF", "ON ON"
-#elif defined(USES_SPDIF_AUDIO)
-#endif
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_RCV[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_RING_SPK[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_RING_NO_MIC[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_HP_NO_MIC[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_RING_HP[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_HP[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_RING_SPK_HP[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_SPK_HP[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_BT[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_out_OFF[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 
 const AudioMixer device_voice_RCV[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_voice_SPK[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_voice_TTY_VCO[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_voice_TTY_HCO[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_voice_TTY_FULL[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_voice_HP_NO_MIC[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_voice_HP[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_voice_BT[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_voice_OFF[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 
 const AudioMixer device_input_Main_Mic[] = {
-#if defined (USES_I2S_AUDIO) || defined (USES_PCM_AUDIO)
-    {"IN2L PGA IN2LN Switch", 1},
-    {"IN2L Switch", 1},
-    {"MIXINL IN2L Switch", 1},
-    {"AIF1ADC1L Mixer ADC/DMIC Switch", 1},
-    {"IN2R PGA IN2RN Switch", 1},
-    {"IN2R Switch", 1},
-    {"MIXINR IN2R Switch", 1},
-    {"AIF1ADC1R Mixer ADC/DMIC Switch", 1},
-    {"AIF1ADC1L Mixer AIF2 Switch", 1},
-    {"AIF1ADC1R Mixer AIF2 Switch", 1},
-#elif defined(USES_SPDIF_AUDIO)
-#endif
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_input_Hands_Free_Mic[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_input_BT_Sco_Mic[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 const AudioMixer device_input_MIC_OFF[] = {
-    {NULL, NULL}
+    {NULL, 0}
 };
 
 }; // namespace android
